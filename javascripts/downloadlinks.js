c0370505df863f711931457e4f2b4133be9f6dac
Martin Potter <675256+martinpotter@users.noreply.github.com>
2023-08-21 11:45:03 -0700

    Update downloadlinks.js

--- javascripts/downloadlinks.js
+++ javascripts/downloadlinks.js
@@ -4,31 +4,31 @@ document.addEventListener("DOMContentLoaded", function(event) {
         var headerElement = versions[i];
 
         var version = headerElement.dataset.version;
-				var platforms = 'platform' in headerElement.dataset ? [ headerElement.dataset.platform ] : [ 'Windows', 'macOS' ];
+                var platforms = 'platform' in headerElement.dataset ? [ headerElement.dataset.platform ] : [ 'Windows', 'macOS' ];
 
         var div = document.createElement('div');
         div.setAttribute('class', 'download-links');
-				for (var j = 0; j < platforms.length; j++)
-				{
-					if (j !== 0)
-						div.appendChild(document.createTextNode(' | '));
+        for (var j = 0; j < platforms.length; j++)
+        {
+            if (j !== 0)
+                div.appendChild(document.createTextNode(' | '));
 
-					var platform = platforms[j];
+            var platform = platforms[j];
 
-					var link = document.createElement('a');
-					link.text = platform;
-					switch (platform)
-					{
-						case 'macOS':
-							link.setAttribute('href', `https://downloads.logoscdn.com/Proclaim/Installer/${version}/Proclaim.dmg`);
-							break;
-						case 'Windows':
-							link.setAttribute('href', `https://downloads.logoscdn.com/Proclaim/Installer/${version}/ProclaimSetup.exe`);
-							break;
-					}
-					
-					div.appendChild(link);
-				}
+            var link = document.createElement('a');
+            link.text = platform;
+            switch (platform)
+            {
+                case 'macOS':
+                    link.setAttribute('href', `https://downloads.logoscdn.com/Proclaim/Installer/${version}/Proclaim.dmg`);
+                    break;
+                case 'Windows':
+                    link.setAttribute('href', `https://downloads.logoscdn.com/Proclaim/Installer/${version}/ProclaimSetup.exe`);
+                    break;
+            }
+            
+            div.appendChild(link);
+        }
 
         headerElement.appendChild(div);
     }
