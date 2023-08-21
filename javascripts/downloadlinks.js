ed295df6dea475819ac9bac31e237e09523ba3c4
Martin Potter <675256+martinpotter@users.noreply.github.com>
2023-08-21 11:41:51 -0700

    Update downloadlinks.js

--- javascripts/downloadlinks.js
+++ javascripts/downloadlinks.js
@@ -4,21 +4,31 @@ document.addEventListener("DOMContentLoaded", function(event) {
         var headerElement = versions[i];
 
         var version = headerElement.dataset.version;
+				var platforms = 'platform' in headerElement.dataset ? [ headerElement.dataset.platform ] : [ 'Windows', 'macOS' ];
 
         var div = document.createElement('div');
         div.setAttribute('class', 'download-links');
+				for (var j = 0; j < platforms.length; j++)
+				{
+					if (j !== 0)
+						div.appendChild(document.createTextNode(' | '));
 
-        var windowsLink = document.createElement('a');
-        windowsLink.text = 'Windows';
-        windowsLink.setAttribute('href', `https://downloads.logoscdn.com/Proclaim/Installer/${version}/ProclaimSetup.exe`);
+					var platform = platforms[j];
 
-        var macLink = document.createElement('a');
-        macLink.text = 'Mac';
-        macLink.setAttribute('href', `https://downloads.logoscdn.com/Proclaim/Installer/${version}/Proclaim.dmg`);
-
-        div.appendChild(windowsLink);
-        div.appendChild(document.createTextNode(' | '));
-        div.appendChild(macLink);
+					var link = document.createElement('a');
+					link.text = platform;
+					switch (platform)
+					{
+						case 'macOS':
+							link.setAttribute('href', `https://downloads.logoscdn.com/Proclaim/Installer/${version}/Proclaim.dmg`);
+							break;
+						case 'Windows':
+							link.setAttribute('href', `https://downloads.logoscdn.com/Proclaim/Installer/${version}/ProclaimSetup.exe`);
+							break;
+					}
+					
+					div.appendChild(link);
+				}
 
         headerElement.appendChild(div);
     }
